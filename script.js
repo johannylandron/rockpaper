19341c6ea06468e8e129cc90cc5dd1a78aa44906
Chris Macke <chrismacke@mac.com>
2016-02-24 15:34:52 -0500

    New files

--- script.js
+++ script.js
@@ -1,38 +1,73 @@
+      $(document).ready(function() {
+      
+        // ****** STEP ONE ******
+        // attach a click event listener here
+        // to your play button (get its ID at the bottom of this page)
+        // and attach it to the play function
+      
+      });
+      
+      ////////////////////////////////////////////////
+      // these are helper functions, don't touch!
+      ////////////////////////////////////////////////
+      function getRandomPlay(){
+        var outcomeArray = ["Rock", "Paper", "Scissors"];
+        return outcomeArray[ Math.floor(Math.random() * outcomeArray.length) ];
+      }
+      
+      function resetFaces(){
+        $(".girl").removeClass("girl-lose"); 
+        $(".boy").removeClass("boy-lose");  
+      }
+      ////////////////////////////////////////////////
+      // end of helper functions
+      ////////////////////////////////////////////////
+      
+      
+      function play(){
+        
+        resetFaces();
+        
+        var boyPlayed = getRandomPlay();
+        var girlPlayed = getRandomPlay();
+        
+        $("#girlResult").html(boyPlayed);
+        $("#boyResult").html(girlPlayed);
+  
+        if(boyPlayed == girlPlayed){
+        
+          tie(); 
+        
+        } else{
 
-//  STEP ONE: 
-//  Attach a click event listener to the answer button that will
-//  execute the checkAnswer function.
-
-$(document).ready(function() {
-
-  // attach your button click event listener here
-
-});
-
-function checkAnswer(){
-  
-  var answer =  $("input[name=question-one]:checked").val();
-  //input[name='ans3']:checked
-  console.log(answer);
-  
-  
-  // STEP TWO:
-  // with an if/else statement, check to see if the answer is correct      
-  // if the answer is correct, use jQuery to add the class "question-correct" to the
-  // div with the id of "question-one", otherwise, add the class "question-incorrect"
-  
-  
-  
-  
-  // STEP THREE:
-  // use a jQuery effect to slideDown the appropriate "remark-correct" or "remark-incorrect"
-  
-  
-  
-  
-  // STEP FOUR (extra credit):
-  // use jQuery to display the reset button once the user has made a selection (this code should be outside of the if/else statement)
-  // create a function called resetQuiz that will remove any styles that we added
-  // create an click event listener for the reset button and attach it to the resetQuiz function
-  
-}
+          // ****** STEP TWO ******
+          // there are three conditions where the boy could win
+          // 1. if boy played Rock and girl played Scissors
+          // 2. if boy played Paper and girl played Rock
+          // 3. if boy played Scissors and girl played Paper
+          
+          // write an if / if else statements testing for these
+          // conditions. If the boy wins, run the function boyWins(),
+          // If the girl wins, run the function girlWins()
+          // hints: == tests if two things are equal, && means "and"
+          // if (boyPlayed == "Rock" && girlPlayed == "Scissors") {}
+          
+        }
+              
+      }
+      
+      function tie(){
+        $("#message").html("It's a tie. Try again.");  
+      }
+      
+      function boyWins(){
+        // ****** STEP THREE ******
+        // add the class "girl-lose" to the girl div
+        $("#message").html("Boy wins!");
+      }
+      
+      function girlWins(){
+        // ****** STEP THREE ******
+        // add the class "boy-lose" to the boy div  
+        $("#message").html("Girl wins!");
+      }
